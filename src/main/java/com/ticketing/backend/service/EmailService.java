049927673c6b78703aa4b9ca7763a19a8e63a764
Kamal Satwani <satwanikamal2003@gmail.com>
2025-12-07 04:53:03 +0530

    async email calls

--- src/main/java/com/ticketing/backend/service/EmailService.java
+++ src/main/java/com/ticketing/backend/service/EmailService.java
@@ -3,6 +3,7 @@ package com.ticketing.backend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,18 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Async
     public void sendEmail(String to, String subject, String message) {
+
+        System.out.println("📧 Sending email asynchronously...");
+
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setTo(to);
         mail.setSubject(subject);
         mail.setText(message);
+
         mailSender.send(mail);
+
+        System.out.println("✅ Email sent successfully!");
     }
 }
